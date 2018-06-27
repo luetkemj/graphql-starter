7e03b1d7f0146bec0e045dd15a1faecc8562809c
luetkemj <luetkemj@gmail.com>
2018-06-26 19:35:07 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
+
+const resolvers = {
+  Query,
+  Mutation,
+  AuthPayload
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: process.env.PRISMA_ENDPOINT,
+      secret: process.env.DB_SECRET,
+      debug: true,
+    }),
+  }),
+});
+
+server.start(() => console.log('Server is running on port 4000'));
